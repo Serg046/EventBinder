d774b65cebd822435f7abd3de52ac701c24d1e48
Serg046 <serg046@outlook.com>
2019-01-12 21:07:44 +0300

    Support Action<object> and Action as alternative to ICommand

---
+++ EventBinder.Tests/SyncTests.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using Moq;
+using Xunit;
+
+namespace EventBinder.Tests
+{
+    public class SyncTests
+    {
+        [Fact]
+        public void SyncProperty_Binding_Fails()
+        {
+            var obj = new DependencyObject();
+
+            Assert.Throws<InvalidCastException>(() => BindingOperations
+                .SetBinding(obj, Sync.CommandProperty, new Binding {Source = Mock.Of<ICommand>()}));
+        }
+
+        [Fact]
+        public void SyncProperty_NoEvent_Fails()
+        {
+            var obj = new UIElement();
+
+            Assert.Throws<InvalidOperationException>(() => BindingOperations.SetBinding(obj,
+                Sync.CommandProperty, new EventBinding { Source = Mock.Of<ICommand>(), EventPath = "IncorrectEvent"}));
+        }
+
+        [WpfTheory]
+        [MemberData(nameof(ValidEventData))]
+        public void SyncProperty_CommandBinding_CommandExecuted(string eventName, Func<DependencyObject, RoutedEventArgs> args)
+        {
+            var testValue = -1;
+            var btn = new Button();
+            btn.SetValue(Sync.CommandParameterProperty, 5);
+            var commandMock = new Mock<ICommand>();
+            commandMock.Setup(c => c.Execute(5)).Callback(() => testValue = 7);
+
+            BindingOperations.SetBinding(btn, Sync.CommandProperty,
+                new EventBinding {Source = commandMock.Object, EventPath = eventName });
+            btn.RaiseEvent(args(btn));
+
+            Assert.Equal(7, testValue);
+            commandMock.VerifyAll();
+        }
+
+        [WpfTheory]
+        [MemberData(nameof(ValidEventData))]
+        public void SyncProperty_CommandBinding_EnabledStateUpdated(string eventName, Func<DependencyObject, RoutedEventArgs> args)
+        {
+            var isEnabled = false;
+            var btn = new Button();
+            btn.SetValue(Sync.CommandParameterProperty, 5);
+            var commandMock = new Mock<ICommand>();
+            commandMock.Setup(c => c.CanExecute(5)).Returns(() => isEnabled);
+
+            BindingOperations.SetBinding(btn, Sync.CommandProperty,
+                new EventBinding { Source = commandMock.Object, EventPath = eventName });
+            btn.RaiseEvent(args(btn));
+
+            Assert.False(btn.IsEnabled);
+            isEnabled = true;
+            commandMock.Raise(c => c.CanExecuteChanged += null, EventArgs.Empty);
+            Assert.True(btn.IsEnabled);
+            commandMock.VerifyAll();
+        }
+
+        [WpfTheory]
+        [MemberData(nameof(ValidEventData))]
+        public void SyncProperty_PrmActionBinding_ActionExecuted(string eventName, Func<DependencyObject, RoutedEventArgs> args)
+        {
+            var testValue = -1;
+            var btn = new Button();
+            btn.SetValue(Sync.ActionParameterProperty, 5);
+            Action<object> action = parameter =>
+            {
+                Assert.Equal(5, parameter);
+                testValue = 7;
+            };
+
+            BindingOperations.SetBinding(btn, Sync.PrmActionProperty,
+                new EventBinding { Source = action, EventPath = eventName });
+            btn.RaiseEvent(args(btn));
+
+            Assert.Equal(7, testValue);
+        }
+
+        [WpfTheory]
+        [MemberData(nameof(ValidEventData))]
+        public void SyncProperty_ActionBinding_ActionExecuted(string eventName, Func<DependencyObject, RoutedEventArgs> args)
+        {
+            var testValue = -1;
+            var btn = new Button();
+            btn.SetValue(Sync.ActionParameterProperty, 5);
+            Action action = () => testValue = 7;
+
+            BindingOperations.SetBinding(btn, Sync.ActionProperty,
+                new EventBinding { Source = action, EventPath = eventName });
+            btn.RaiseEvent(args(btn));
+
+            Assert.Equal(7, testValue);
+        }
+
+        public static IEnumerable<object[]> ValidEventData()
+        {
+            Func<DependencyObject, RoutedEventArgs> eventArgs = source => new RoutedEventArgs(ButtonBase.ClickEvent, source);
+            yield return new object[] { "Click", eventArgs };
+
+            eventArgs = source => new MouseButtonEventArgs(Mouse.PrimaryDevice, 0, MouseButton.Right)
+            {
+                RoutedEvent = UIElement.MouseRightButtonDownEvent,
+                Source = source
+            };
+            yield return new object[] { "MouseRightButtonDown", eventArgs };
+
+            eventArgs = source => (ContextMenuEventArgs)Activator.CreateInstance(typeof(ContextMenuEventArgs),
+                BindingFlags.CreateInstance | BindingFlags.NonPublic | BindingFlags.Instance,
+                null, new object[] { source, true }, CultureInfo.CurrentCulture);
+            yield return new object[] { "ContextMenuOpening", eventArgs };
+
+            eventArgs = source => new KeyEventArgs(Keyboard.PrimaryDevice,
+                Mock.Of<PresentationSource>(), 0, Key.Enter)
+            {
+                RoutedEvent = UIElement.KeyUpEvent,
+                Source = source
+            };
+            yield return new object[] { "KeyUp", eventArgs };
+        }
+    }
+}
---
+++ EventBinder/Sync.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using LinqExpr = System.Linq.Expressions;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace EventBinder
+{
+    public static class Sync
+    {
+        public static readonly DependencyProperty CommandProperty = DependencyProperty.RegisterAttached("Command",
+            typeof(ICommand), typeof(Sync), new PropertyMetadata(CommandPropertyChangedCallback));
+        public static ICommand GetCommand(DependencyObject element) => (ICommand)element.GetValue(CommandProperty);
+        public static void SetCommand(DependencyObject element, ICommand value) => element.SetValue(CommandProperty, value);
+
+        public static readonly DependencyProperty CommandParameterProperty = DependencyProperty.RegisterAttached("CommandParameter",
+            typeof(object), typeof(Sync));
+        public static object GetCommandParameter(DependencyObject element) => element.GetValue(CommandParameterProperty);
+        public static void SetCommandParameter(DependencyObject element, object value) => element.SetValue(CommandParameterProperty, value);
+
+        private static void CommandPropertyChangedCallback(DependencyObject parent, DependencyPropertyChangedEventArgs args)
+        {
+            var command = (ICommand)args.NewValue;
+            Subscribe(parent, args, () => command.Execute(parent.GetValue(CommandParameterProperty)));
+
+            if (parent is UIElement uiElement)
+            {
+                UpdateState();
+                command.CanExecuteChanged += (sender, eventArgs) => UpdateState();
+                void UpdateState() => uiElement.IsEnabled = command.CanExecute(parent.GetValue(CommandParameterProperty));
+            }
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------
+
+        public static readonly DependencyProperty PrmActionProperty = DependencyProperty.RegisterAttached("PrmAction",
+            typeof(Action<object>), typeof(Sync), new PropertyMetadata(PrmActionPropertyChangedCallback));
+        public static Action<object> GetPrmAction(DependencyObject element) => (Action<object>)element.GetValue(PrmActionProperty);
+        public static void SetPrmAction(DependencyObject element, Action<object> value) => element.SetValue(PrmActionProperty, value);
+
+        public static readonly DependencyProperty ActionParameterProperty = DependencyProperty.RegisterAttached("ActionParameter",
+            typeof(object), typeof(Sync));
+        public static object GetActionParameter(DependencyObject element) => element.GetValue(ActionParameterProperty);
+        public static void SetActionParameter(DependencyObject element, object value) => element.SetValue(ActionParameterProperty, value);
+
+        private static void PrmActionPropertyChangedCallback(DependencyObject parent, DependencyPropertyChangedEventArgs args)
+        {
+            var action = (Action<object>)args.NewValue;
+            Subscribe(parent, args, () => action.Invoke(parent.GetValue(ActionParameterProperty)));
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------
+
+        public static readonly DependencyProperty ActionProperty = DependencyProperty.RegisterAttached("Action",
+            typeof(Action), typeof(Sync), new PropertyMetadata(ActionPropertyChangedCallback));
+        public static Action GetAction(DependencyObject element) => (Action)element.GetValue(ActionProperty);
+        public static void SetAction(DependencyObject element, Action value) => element.SetValue(ActionProperty, value);
+
+        private static void ActionPropertyChangedCallback(DependencyObject parent, DependencyPropertyChangedEventArgs args)
+        {
+            var action = (Action)args.NewValue;
+            Subscribe(parent, args, () => action());
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------
+
+        private static void Subscribe(DependencyObject parent, DependencyPropertyChangedEventArgs args, LinqExpr.Expression<Action> callExpression)
+        {
+            var binding = (EventBinding)BindingOperations.GetBinding(parent, args.Property)
+                          ?? throw new InvalidOperationException("Cannot get binding");
+            var eventInfo = parent.GetType().GetEvent(binding.EventPath)
+                            ?? throw new InvalidOperationException($"Cannot find the event '{binding.EventPath}'");
+            var parameters = eventInfo.EventHandlerType.GetMethods().Single(m => m.Name == "Invoke").GetParameters();
+
+            var methodExpression = LinqExpr.Expression.Lambda(eventInfo.EventHandlerType, callExpression.Body,
+                parameters.Select(p => LinqExpr.Expression.Parameter(p.ParameterType)));
+            var method = methodExpression.Compile();
+
+            eventInfo.AddEventHandler(parent, method);
+        }
+    }
+}
