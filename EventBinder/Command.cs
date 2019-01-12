8ba1323cd752ede7d44570697354e97f6931c8e5
Serg046 <serg046@outlook.com>
2019-01-12 03:26:47 +0300

    Add project files.

---
+++ EventBinder/Command.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using LinqExpr = System.Linq.Expressions;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace EventBinder
+{
+    public static class Command
+    {
+        public static readonly DependencyProperty SyncProperty = DependencyProperty.RegisterAttached("Sync",
+            typeof(ICommand), typeof(Command), new PropertyMetadata(SyncPropertyChangedCallback));
+        public static ICommand GetSync(DependencyObject element) => (ICommand)element.GetValue(SyncProperty);
+        public static void SetSync(DependencyObject element, ICommand value) => element.SetValue(SyncProperty, value);
+
+        public static readonly DependencyProperty SyncParameterProperty = DependencyProperty.RegisterAttached("SyncParameter",
+            typeof(object), typeof(Command));
+        public static object GetSyncParameter(DependencyObject element) => element.GetValue(SyncParameterProperty);
+        public static void SetSyncParameter(DependencyObject element, object value) => element.SetValue(SyncParameterProperty, value);
+
+        private static void SyncPropertyChangedCallback(DependencyObject parent, DependencyPropertyChangedEventArgs args)
+        {
+            var binding = (EventBinding)BindingOperations.GetBinding(parent, args.Property)
+                          ?? throw new InvalidOperationException("Cannot get binding");
+            var eventInfo = parent.GetType().GetEvent(binding.EventPath)
+                            ?? throw new InvalidOperationException($"Cannot find the event '{binding.EventPath}'");
+            var parameters = eventInfo.EventHandlerType.GetMethods().First().GetParameters();
+
+            var command = (ICommand)args.NewValue;
+            LinqExpr.Expression<Action> executeExpression = () => command.Execute(parent.GetValue(SyncParameterProperty));
+            var methodExpression = LinqExpr.Expression.Lambda(eventInfo.EventHandlerType, executeExpression.Body,
+                parameters.Select(p => LinqExpr.Expression.Parameter(p.ParameterType)));
+            var method = methodExpression.Compile();
+
+            eventInfo.AddEventHandler(parent, method);
+
+            if (parent is UIElement uiElement)
+            {
+                UpdateState();
+                command.CanExecuteChanged += (sender, eventArgs) => UpdateState();
+                void UpdateState() => uiElement.IsEnabled = command.CanExecute(parent.GetValue(SyncParameterProperty));
+            }
+        }
+    }
+}
---
+++ EventBinder/EventBinding.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace EventBinder
+{
+    public class EventBinding : Binding
+    {
+        private string _eventPath;
+
+        public EventBinding() : base() {}
+        public EventBinding(string path, string eventPath) : base(path) => EventPath = eventPath ?? throw new ArgumentNullException(nameof(eventPath));
+
+        public string EventPath
+        {
+            get => _eventPath;
+            set => _eventPath = value ?? throw new ArgumentNullException(nameof(EventPath));
+        }
+    }
+}
