8c6542466f8584283ef5e85637f4ff19209b5f4d
Serg046 <serg046@outlook.com>
2019-08-12 17:45:20 +0300

    Include missing Xunit.StaFact nuget

--- EventBinder.Tests/MultiBindTests.cs
+++ EventBinder.Tests/MultiBindTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
