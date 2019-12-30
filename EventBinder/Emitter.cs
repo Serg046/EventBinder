d9d31078176c5a91318c2df8de97fe1bb4e0575f
Serg046 <serg046@outlook.com>
2019-12-31 01:11:57 +0300

    Refactor EventHandlerGenerator

---
+++ EventBinder/Emitter.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace EventBinder
+{
+	internal class Emitter
+	{
+        internal const string HANDLER_METHOD_NAME = "Handle";
+
+		private readonly TypeBuilder _typeBuilder;
+		private readonly string _methodPath;
+		private readonly FrameworkElement _source;
+		private readonly FieldInfo _instanceField;
+		private readonly FieldInfo _argumentsField;
+		private static readonly DependencyPropertyCollection _properties = new DependencyPropertyCollection("Argument");
+
+		public Emitter(TypeBuilder typeBuilder, string methodPath, FrameworkElement source)
+		{
+			_typeBuilder = typeBuilder;
+			_methodPath = methodPath;
+			_source = source;
+			_instanceField = _typeBuilder.DefineField("_instance", source.GetType(), FieldAttributes.Private);
+			_argumentsField = _typeBuilder.DefineField("_arguments", typeof(object[]), FieldAttributes.Private);
+		}
+
+		public void GenerateHandler(object[] arguments, Type[] parameterTypes)
+		{
+			GenerateCtor();
+			var method = _typeBuilder.DefineMethod(HANDLER_METHOD_NAME, MethodAttributes.Public, typeof(void), parameterTypes);
+			var body = method.GetILGenerator();
+			body.Emit(OpCodes.Ldarg_0);
+			body.Emit(OpCodes.Ldfld, _instanceField);
+			var dataContextProp = _source.GetType().GetProperty(nameof(FrameworkElement.DataContext));
+			body.Emit(OpCodes.Call, dataContextProp.GetGetMethod());
+			var argumentTypes = HandleArguments(arguments, parameterTypes, body);
+			var innerMethod = _source.DataContext.GetType().GetMethod(_methodPath, argumentTypes);
+			if (innerMethod == null) ThrowMissingMethodException(_methodPath, argumentTypes);
+			body.Emit(OpCodes.Callvirt, innerMethod);
+			if (innerMethod.ReturnType != typeof(void))
+			{
+				body.Emit(OpCodes.Pop);
+			}
+			body.Emit(OpCodes.Ret);
+		}
+
+		private Type[] HandleArguments(object[] arguments, Type[] parameterTypes, ILGenerator body)
+		{
+			var argumentTypes = new Type[arguments.Length];
+			for (var i = 0; i < arguments.Length; i++)
+			{
+				var argument = arguments[i];
+				Type argumentType;
+				if (argument is EventArg eventArg)
+				{
+					argumentType = HandleEventArg(parameterTypes, body, eventArg);
+				}
+				else
+				{
+					argumentType = HandleArg(body, i, argument);
+				}
+				if (argument is Binding binding)
+				{
+					argumentType = HandleBindingArg(binding, body, i);
+				}
+				argumentTypes[i] = argumentType;
+			}
+			return argumentTypes;
+		}
+
+		private Type HandleArg(ILGenerator body, int position, object argument)
+		{
+			body.Emit(OpCodes.Ldarg_0);
+			body.Emit(OpCodes.Ldfld, _argumentsField);
+			body.Emit(OpCodes.Ldc_I4, position);
+			body.Emit(OpCodes.Ldelem_Ref);
+			var argumentType = argument.GetType();
+			if (argumentType.IsValueType)
+			{
+				body.Emit(OpCodes.Unbox_Any, argumentType);
+			}
+			return argumentType;
+		}
+
+		private static Type HandleEventArg(Type[] parameterTypes, ILGenerator body, EventArg eventArg)
+		{
+			body.Emit(OpCodes.Ldarg, eventArg.Position + 1);
+			if (parameterTypes.Length <= eventArg.Position)
+				throw new ArgumentOutOfRangeException($"{eventArg.OriginalString} is not available");
+			var argumentType = parameterTypes[eventArg.Position];
+			if (argumentType.IsValueType)
+			{
+				body.Emit(OpCodes.Unbox_Any, argumentType);
+			}
+			return argumentType;
+		}
+
+		private Type HandleBindingArg(Binding binding, ILGenerator body, int position)
+		{
+			var parent = GetParent(_source) as FrameworkElement;
+			var obj = parent.FindName(binding.ElementName) as DependencyObject;
+			var propType = obj.GetType().GetProperty(binding.Path.Path).PropertyType;
+			body.Emit(OpCodes.Ldarg_0);
+			body.Emit(OpCodes.Ldfld, _instanceField);
+			body.Emit(OpCodes.Ldc_I4, position);
+			var resolveBindingMethod = GetType().GetMethod(nameof(ResolveBinding), BindingFlags.NonPublic | BindingFlags.Static);
+			body.Emit(OpCodes.Call, resolveBindingMethod);
+			if (propType != typeof(object))
+			{
+				body.Emit(propType.IsValueType ? OpCodes.Unbox_Any : OpCodes.Castclass, propType);
+			}
+			return propType;
+		}
+
+		internal static object ResolveBinding(Binding binding, FrameworkElement source, int position)
+		{
+			if (!BindingOperations.IsDataBound(source, _properties[position]))
+			{
+				BindingOperations.SetBinding(source, _properties[position], binding);
+			}
+			return source.GetValue(_properties[position]);
+		}
+
+		private void GenerateCtor()
+		{
+			var ctor = _typeBuilder.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard,
+				new[] { _instanceField.FieldType, _argumentsField.FieldType });
+			var body = ctor.GetILGenerator();
+			body.Emit(OpCodes.Ldarg_0);
+			body.Emit(OpCodes.Ldarg_1);
+			body.Emit(OpCodes.Stfld, _instanceField);
+			body.Emit(OpCodes.Ldarg_0);
+			body.Emit(OpCodes.Ldarg_2);
+			body.Emit(OpCodes.Stfld, _argumentsField);
+			body.Emit(OpCodes.Ret);
+		}
+
+		private static void ThrowMissingMethodException(string methodPath, Type[] argumentTypes)
+		{
+			var sb = new StringBuilder("Cannot find ")
+				.Append(methodPath)
+				.Append("(");
+			for (var i = 0; i < argumentTypes.Length; i++)
+			{
+				sb.Append(argumentTypes[i].Name);
+				if (i < argumentTypes.Length - 1)
+				{
+					sb.Append(",");
+				}
+			}
+			sb.Append(")");
+			throw new MissingMethodException(sb.ToString());
+		}
+
+		private DependencyObject GetParent(DependencyObject obj)
+		{
+			var parent = VisualTreeHelper.GetParent(obj);
+			return parent != null ? GetParent(parent) : obj;
+		}
+
+		internal class EventArg
+		{
+			public EventArg(string originalString, ushort position)
+			{
+				OriginalString = originalString;
+				Position = position;
+			}
+
+			public string OriginalString { get; }
+			public ushort Position { get; }
+		}
+	}
+}
--- EventBinder/EventHandlerGenerator.cs
+++ EventBinder/EventHandlerGenerator.cs
@@ -5,16 +5,12 @@ using System.Reflection;
 using System.Reflection.Emit;
 using System.Text;
 using System.Windows;
-using System.Windows.Data;
-using System.Windows.Media;
 
 namespace EventBinder
 {
     internal class EventHandlerGenerator
     {
-        private const string HANDLER_METHOD_NAME = "Handle";
         private readonly ModuleBuilder _module;
-        private static readonly DependencyPropertyCollection _properties = new DependencyPropertyCollection("Argument");
 		private readonly Dictionary<Type, Delegate> _emptyHandlerCache = new Dictionary<Type, Delegate>();
 		private readonly Dictionary<string, Type> _handlerCache = new Dictionary<string, Type>();
 
@@ -41,16 +37,14 @@ namespace EventBinder
 			if (!_handlerCache.TryGetValue(key, out var handlerType))
             {
 	            var type = _module.DefineType(Guid.NewGuid().ToString(), TypeAttributes.Public);
-	            var instanceFld = type.DefineField("_instance", source.GetType(), FieldAttributes.Private);
-	            var argumentsFld = type.DefineField("_arguments", typeof(object[]), FieldAttributes.Private);
-	            GenerateCtor(type, instanceFld, argumentsFld);
-	            GenerateHandler(binding.MethodPath, arguments, source, instanceFld, argumentsFld, type, parameterTypes);
+				var emitter = new Emitter(type, binding.MethodPath, source);
+				emitter.GenerateHandler(arguments, parameterTypes);
 	            handlerType = type.CreateType();
 				_handlerCache.Add(key, handlerType);
 			}
 
 			var instance = Activator.CreateInstance(handlerType, new object[] { source, arguments });
-            return Delegate.CreateDelegate(eventHandler, instance, HANDLER_METHOD_NAME);
+            return Delegate.CreateDelegate(eventHandler, instance, Emitter.HANDLER_METHOD_NAME);
         }
 
         private string GetKey(Type eventHandler, string methodPath, object[] arguments)
@@ -75,116 +69,6 @@ namespace EventBinder
             return parameterTypes;
         }
 
-        private void GenerateHandler(string methodPath, object[] arguments, FrameworkElement source, FieldBuilder instanceFld,
-            FieldBuilder argumentsFld, TypeBuilder typeBuilder, Type[] parameterTypes)
-        {
-            var method = typeBuilder.DefineMethod(HANDLER_METHOD_NAME, MethodAttributes.Public, typeof(void), parameterTypes);
-            var body = method.GetILGenerator();
-            body.Emit(OpCodes.Ldarg_0);
-            body.Emit(OpCodes.Ldfld, instanceFld);
-            var dataContextProp = source.GetType().GetProperty(nameof(FrameworkElement.DataContext));
-            body.Emit(OpCodes.Call, dataContextProp.GetGetMethod());
-            var argumentTypes = new Type[arguments.Length];
-            for (var i = 0; i < arguments.Length; i++)
-            {
-                var argument = arguments[i];
-                Type argumentType;
-                if (argument is EventArg eventArg)
-                {
-                    body.Emit(OpCodes.Ldarg, eventArg.Position + 1);
-                    if (parameterTypes.Length <= eventArg.Position) throw new ArgumentOutOfRangeException($"{eventArg.OriginalString} is not available");
-                    argumentType = parameterTypes[eventArg.Position];
-                    if (argumentType.IsValueType)
-                    {
-                        body.Emit(OpCodes.Unbox_Any, argumentType);
-                    }
-                }
-                else
-                {
-                    body.Emit(OpCodes.Ldarg_0);
-                    body.Emit(OpCodes.Ldfld, argumentsFld);
-                    body.Emit(OpCodes.Ldc_I4, i);
-                    body.Emit(OpCodes.Ldelem_Ref);
-                    argumentType = argument.GetType();
-                    if (argumentType.IsValueType)
-                    {
-                        body.Emit(OpCodes.Unbox_Any, argumentType);
-                    }
-                }
-                if (argument is Binding binding)
-                {
-					var parent = GetParent(source) as FrameworkElement;
-					var obj = parent.FindName(binding.ElementName) as DependencyObject;
-					var propType = obj.GetType().GetProperty(binding.Path.Path).PropertyType;
-					body.Emit(OpCodes.Ldarg_0);
-                    body.Emit(OpCodes.Ldfld, instanceFld);
-                    body.Emit(OpCodes.Ldc_I4, i);
-                    var resolveBindingMethod = GetType().GetMethod(nameof(ResolveBinding), BindingFlags.NonPublic | BindingFlags.Static);
-                    body.Emit(OpCodes.Call, resolveBindingMethod);
-                    if (propType != typeof(object))
-                    {
-	                    body.Emit(propType.IsValueType ? OpCodes.Unbox_Any : OpCodes.Castclass, propType);
-                    }
-                    argumentType = propType;
-                }
-                argumentTypes[i] = argumentType;
-            }
-            var innerMethod = source.DataContext.GetType().GetMethod(methodPath, argumentTypes);
-            if (innerMethod == null) ThrowMissingMethodException(methodPath, argumentTypes);
-            body.Emit(OpCodes.Callvirt, innerMethod);
-            if (innerMethod.ReturnType != typeof(void))
-            {
-                body.Emit(OpCodes.Pop);
-            }
-            body.Emit(OpCodes.Ret);
-        }
-
-        private DependencyObject GetParent(DependencyObject obj)
-        {
-            var parent = VisualTreeHelper.GetParent(obj);
-            return parent != null ? GetParent(parent) : obj;
-        }
-
-        internal static object ResolveBinding(Binding binding, FrameworkElement source, int position)
-        {
-            if (!BindingOperations.IsDataBound(source, _properties[position]))
-            {
-                BindingOperations.SetBinding(source, _properties[position], binding);
-            }
-            return source.GetValue(_properties[position]);
-        }
-
-        private static void ThrowMissingMethodException(string methodPath, Type[] argumentTypes)
-        {
-            var sb = new StringBuilder("Cannot find ")
-                .Append(methodPath)
-                .Append("(");
-            for (var i = 0; i < argumentTypes.Length; i++)
-            {
-                sb.Append(argumentTypes[i].Name);
-                if (i < argumentTypes.Length - 1)
-                {
-                    sb.Append(",");
-                }
-            }
-            sb.Append(")");
-            throw new MissingMethodException(sb.ToString());
-        }
-
-        private static void GenerateCtor(TypeBuilder type, FieldBuilder instanceFld, FieldBuilder argumentsFld)
-        {
-            var ctor = type.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard,
-                            new[] { instanceFld.FieldType, argumentsFld.FieldType });
-            var body = ctor.GetILGenerator();
-            body.Emit(OpCodes.Ldarg_0);
-            body.Emit(OpCodes.Ldarg_1);
-            body.Emit(OpCodes.Stfld, instanceFld);
-            body.Emit(OpCodes.Ldarg_0);
-            body.Emit(OpCodes.Ldarg_2);
-            body.Emit(OpCodes.Stfld, argumentsFld);
-            body.Emit(OpCodes.Ret);
-        }
-
         private object[] ResolveArguments(object[] arguments)
         {
             var result = new object[arguments.Length];
@@ -205,7 +89,7 @@ namespace EventBinder
                         {
                             if (str[0] == '$' && ushort.TryParse(str.Substring(1), NumberStyles.Any, CultureInfo.InvariantCulture, out var position))
                             {
-                                return new EventArg(str, position);
+                                return new Emitter.EventArg(str, position);
                             }
                             else if (str[str.Length - 1] == 'm' && decimal.TryParse(str.Substring(0, str.Length - 1), NumberStyles.Any, CultureInfo.InvariantCulture, out var dec))
                             {
@@ -228,18 +112,6 @@ namespace EventBinder
                     }
                 default: return argument;
             }
-        }
-
-        private class EventArg
-        {
-            public EventArg(string originalString, ushort position)
-            {
-                OriginalString = originalString;
-                Position = position;
-            }
-
-            public string OriginalString { get; }
-            public ushort Position { get; }
         }
     }
 }
