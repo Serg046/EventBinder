b83bf1c482e274802eb9a24828e65aa3adcd1c73
Serg046 <serg046@outlook.com>
2021-05-07 22:08:49 +0300

    v2.5.0.1

--- EventBinder.Avalonia/AssemblyInfo.cs
+++ EventBinder.Avalonia/AssemblyInfo.cs
@@ -1,7 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using EventBinder;
 
-[assembly: AssemblyVersion("2.4.1.0")]
-[assembly: AssemblyFileVersion("2.4.1.0")]
 [assembly: InternalsVisibleTo(EventBinding.ASSEMBLY_NAME)]
--- EventBinder/Properties/AssemblyInfo.cs
+++ EventBinder/Properties/AssemblyInfo.cs
@@ -1,19 +1,5 @@
 ﻿using EventBinder;
-using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("EventBinder")]
-[assembly: AssemblyDescription("Allows to bind a method to any event")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EventBinder")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("539e63ff-327c-4a83-84f2-784c35152e31")]
-[assembly: AssemblyVersion("2.4.1.0")]
-[assembly: AssemblyFileVersion("2.4.1.0")]
 [assembly: InternalsVisibleTo(EventBinding.ASSEMBLY_NAME)]
 [assembly: InternalsVisibleTo("EventBinder.PerformanceTests")]
